9df166e4e831bf084d1726fa48456ee5e01d95f1
QianLi <1290259180@qq.com>
2017-09-27 14:54:49 +0800

    图书馆样式，积分详情，积分商城

--- react/src/components/personal/pointDetail/PointDetail.jsx
+++ react/src/components/personal/pointDetail/PointDetail.jsx
@@ -7,9 +7,43 @@ class PointDetail extends React.Component{
 	}
 	render(){
 		return(
-			<div className="warpper">
-		
-			</div>
+				<div className="warpper">
+					<div className="am-panel user-jf clearFix">
+						<div className="user-tx-name">
+							<img src={require('../../../style/images/test.png')}/>
+							<p>毛*平</p>
+						</div>
+						<div className="user-jf-details">
+							<p>15060分</p>
+							<div className="progress">
+								<div className="progress-bar" style="width: 30%"></div>
+							</div>
+							<ul className="am-avg-sm-4 am-text-center">
+								<li>高手</li>
+								<li>掌门</li>
+								<li>宗师</li>
+								<li>盟主</li>
+							</ul>
+						</div>
+					</div>
+					<div className="am-panel">
+						<div className="am-alert am-alert-danger" id="my-alert" style="display: none">
+							<p>开始日期应小于结束日期！</p>
+						</div>
+						<div className="datepicker">
+							<label type="button" className="am-margin-right">开始日期</label><span id="my-startDate">年-月-日</span>
+						</div>
+						<div className="datepicker">
+							<label type="button" className="am-margin-right">结束日期</label><span id="my-endDate">年-月-日</span>
+						</div>
+						<button type="button" className="btn-query">查询</button>
+					</div>
+					<div className="am-panel jf-list">
+						<p className="jf-tag">获得</p>
+						<p className="time">2017.06.10  12:00</p>
+						<p className="jf-list-cont">您通过在线学习，获得10点等级积分和用积分。当前等级积分X点，可用积分Y点</p>
+					</div>
+				</div> 
 		)
 	}
 
--- react/src/components/personal/pointShop/PointShop.jsx
+++ react/src/components/personal/pointShop/PointShop.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router'
 import './pointShop.less'
-class pointShop extends React.Component{
+class PointShop extends React.Component{
 	constructor(args){
 		super()
 	}
 	render(){
 		return(
 			<div className="warpper">
-		       
+		       <div className="am-panel goods-list">
+				<div className="goods-img"><img src={require('../../../style/images/test.png')}/></div>
+				<div className="goods-info">
+					<h3>Lamy恒星系列2017限量款太平洋蓝钢笔</h3>
+					<p><label>兑换积分:</label><span>2800</span></p>
+					<p><label>库存:</label><font>80</font></p>
+					<button type="button" className="am-btn-primary" data-am-modal="{target: '#my-confirm'}">兑换</button>
+				</div>
+			   </div>
 			</div>
 		)
 	}
@@ -16,4 +24,4 @@ class pointShop extends React.Component{
 }
 
 
-export default pointShop
+export default PointShop
