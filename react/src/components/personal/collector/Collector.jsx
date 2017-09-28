eab8a67bbb28bccffda5fa0b65aa24016ab906db
QianLi <1290259180@qq.com>
2017-09-28 17:28:51 +0800

    锋行圈收藏

--- react/src/components/personal/collector/Collector.jsx
+++ react/src/components/personal/collector/Collector.jsx
@@ -34,6 +34,44 @@ class CourseCol extends React.Component{
 }
 class EssayCol extends React.Component{
     constructor(args){
+		super()
+        this.state={
+            colCourseList:tools.getObject(10),
+        }
+	}
+    render(){
+        return(
+            <div data-tab-panel-0 className="am-tab-panel am-active tab">
+                {
+                    this.state.colCourseList.map((item,index)=>{
+                        return(
+                           <div key={index} className="am-panel cur-list">
+                                <img src={require("../../../style/images/test.png")}/>
+							<div className="cont">
+								<p className="info"><span>用户B</span>xxxxxxx分公司</p>
+								<p className="time">2016.06.15</p>
+								<a>
+									<article className="am-article">
+									  	<div className="am-article-hd">
+									   		<h1 className="am-article-title"><div className="jc-icon"></div>如何用保险保障自己的一生？</h1>
+									  	</div>
+									  	<div className="am-article-bd">
+									    	<p className="am-article-lead">我写这回答的目的是希望各位有幸看到本文的朋友能抽出您人生中的30分钟尽量一字不拉地读完本…</p>
+									  	</div>
+									</article>
+								</a>
+								<p className="like"><span><i className="fa fa-heart"></i>12331</span><span><i className="fa fa-thumbs-up"></i>12331</span></p>
+							</div>
+						   </div>
+                        )
+                    })
+                }
+             </div>
+        )
+    }
+}
+class EssayCol1 extends React.Component{
+    constructor(args){
 		super();
         this.state={
             ColEssayList:tools.getObject(10)
@@ -41,7 +79,7 @@ class EssayCol extends React.Component{
 	}
     render(){
         return(
-            <div data-tab-panel-1 className="am-tab-panel tab">
+            <div data-tab-panel-0 className="am-tab-panel am-active tab">
                 {
                     this.state.ColEssayList.map((item,index)=>{
                         <div key={index} className="am-panel article-list">
