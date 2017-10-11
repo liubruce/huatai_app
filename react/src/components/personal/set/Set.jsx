3638489e001b87689c4354d4bf0ff14d704e8e0b
JiChuangWei <2323176669@qq.com>
2017-10-11 15:14:00 +0800

    Set font-size icon

--- react/src/components/personal/set/Set.jsx
+++ react/src/components/personal/set/Set.jsx
@@ -32,13 +32,13 @@ class Set extends React.Component {
   }
 	render() {
 		return (
-	   <div className="warpper">
+	   <div className="warpper set">
         <ul className="am-list am-list-border list-in">
           <li>
-            <a onClick={()=>this.back()} ><i className="icon icon-jfdetils" />返回首页<i className="fa fa-angle-right floatR" /></a>
+            <a onClick={()=>this.back()} ><i className="icon icon-set-up" />返回首页<i className="fa fa-angle-right floatR" /></a>
           </li>
           <li>
-            <a onClick={()=>this.exit()} ><i className="icon icon-jfdetils" />退出登录<i className="fa fa-angle-right floatR" /></a>
+            <a onClick={()=>this.exit()} ><i className="icon icon-set-up" />退出登录<i className="fa fa-angle-right floatR" /></a>
           </li>
         </ul>
       </div>
