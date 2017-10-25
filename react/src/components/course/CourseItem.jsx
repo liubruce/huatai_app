60939321583b0ca55c974401f62b6587ff729a55
QianLi <1290259180@qq.com>
2017-10-25 19:27:12 +0800

    课程

--- react/src/components/course/CourseItem.jsx
+++ react/src/components/course/CourseItem.jsx
@@ -68,45 +68,6 @@ class CourseItem extends React.Component {
 		let item = this.props.item;
 		let isBuy = item.goodCourse ==='1' && item.userCourseOperation.isBuy !== 1;
 		return(
-            /*<div className="course-item">
-              <div data-tab-panel-0 className="am-tab-panel am-active tab">
-                <div className="am-panel cur-list">
-                 <a>
-                    <video 
-                    onClick={()=>this.jump(item)} data-am-modal={isBuy?`{target: '#course-confirm${item.courseId}'}`:""}
-                    src={getFile_IP+'/downfile/'+item.coursevideoPath}   
-                    className="v-img"
-                    >
-                    您的浏览器不支持 video 标签。
-                   </video>
-                    <div className="right">
-                  
-                    	<p onClick={()=>this.jump(item)} data-am-modal={isBuy?`{target: '#course-confirm${item.courseId}'}`:""} className="time">{tool.formatTimestamp(item.createTime)}</p>
-                    	<h2 onClick={()=>this.jump(item)} data-am-modal={isBuy?`{target: '#course-confirm${item.courseId}'}`:""} >{item.courseName}{item.goodCourse === '1'?<div className="jc-icon" />:null}</h2>
-                    
-                      <p className="like">
-                      <span className={item.userCourseOperation.isCollection===1?'active':''} data-am-modal={isBuy?`{target: '#course-confirm${item.courseId}'}`:""} onClick={()=>this.click(item)} ><i className="fa fa-heart-o" />{item.sumCollection}</span>
-                      <span className={item.userCourseOperation.isLike===1?'active':''} onClick={()=>this.action(item.courseId,3)} ><i className="fa fa-thumbs-o-up" />{item.sumLike}</span></p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-        
-
-      <div className="am-modal am-modal-confirm" tabIndex="-1" id={`course-confirm${item.courseId}`}>
-       <div className="am-modal-dialog">
-         <div className="am-modal-hd">温馨提示</div>
-         <div className="am-modal-bd">
-           兑换课程将需要{this.state.now_item.studyIntegral}积分，您的当前积分为{this.props.score}，是否继续？
-         </div>
-         <div className="am-modal-footer">
-           <span className="am-modal-btn" data-am-modal-cancel>取消</span>
-           <span className="am-modal-btn" data-am-modal-confirm onClick={()=>this.ok()}>确定</span>
-         </div>
-        </div>
-       </div>
-       
-              </div>*/
 			  <li>
 						<a>
 							<img onClick={()=>this.jump(item)} data-am-modal={isBuy?`{target: '#course-confirm${item.courseId}'}`:""} src={require('../../style/images/test.png')}/>
