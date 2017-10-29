437853a866144de7d7f304c9cd85508ad8d7d125
li1359987459@163.com <1359987459>
2017-10-29 12:11:25 +0800

    修改

--- react/src/components/personal/Menu.jsx
+++ react/src/components/personal/Menu.jsx
@@ -93,6 +93,7 @@ class Menu extends React.Component {
   sign(){
     api.sign().then((data)=>{
       if (data.result === 'RC100') {
+        message.success("签到成功",3)
           this.setState({
             sign:data.sign
           })
