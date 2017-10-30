d6e5d6c6c169c5ebc3f1ea7ef94d95e758f68009
QianLi <1290259180@qq.com>
2017-10-30 14:16:41 +0800

    课程

--- react/src/components/course/Course.jsx
+++ react/src/components/course/Course.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './course.less'
 import { message , Spin } from 'antd';
-import * as api from '../../config/api'
-import * as tool from '../../config/tools'
-import CourseItem from './CourseItem.jsx'
+import * as api from '../../config/api';
+import * as tool from '../../config/tools';
+import CourseItem from './CourseItem.jsx';
+import {hashHistory} from 'react-router';
 class Course extends React.Component {
   constructor(args) {
     super()
@@ -15,7 +16,10 @@ class Course extends React.Component {
       elecReqCourse: 2,
       goodCourse: '',
       score:0,
-      totalPage:0
+      totalPage:0,
+      now_item: {
+				studyIntegral:0
+			}
     }
   }
   componentDidMount() {
@@ -86,6 +90,59 @@ class Course extends React.Component {
       tool.reject(res);
     })
   }
+
+  jump(item) {
+    console.log(item);
+		if (item.goodCourse !== '1' || item.userCourseOperation.isBuy === 1) {
+			hashHistory.push(`/App/Course/courseDetail/${item.courseId}`);
+			return;
+		}
+		this.setState({
+			now_item: item
+		})
+	}
+  ok() {
+		let body = {
+			courseId: this.state.now_item.courseId
+		}
+		api.cashcourse(body).then((data) => {
+			if (data.result === 'RC100') {
+				hashHistory.push(`/App/Course/courseDetail/${this.state.now_item.courseId}`);
+			} else {
+				message.error(data.errMsg, 1);
+			}
+			tool.loading(this, false);
+		}, (res) => {
+			tool.loading(this, false);
+			tool.reject(res);
+		})
+	}
+  action(courseId,recordType){
+		let body = {
+			courseId,
+			recordType
+		};
+		api.operatecourse(body).then((data) => {
+			if (data.result === 'RC100') {
+				this.show();
+			} else {
+				message.error(data.errMsg, 1);
+			}
+			tool.loading(this, false);
+		}, (res) => {
+			tool.loading(this, false);
+			tool.reject(res);
+		})
+	}
+  click(item){
+		if (item.goodCourse !== '1' || item.userCourseOperation.isBuy === 1) {
+			this.action(item.courseId,2);
+			return;
+		}
+		this.setState({
+			now_item: item
+		})
+	}
 	render(){
 		return(
              <div className="warpper">
@@ -108,13 +165,38 @@ class Course extends React.Component {
 					             	<ul className="cur-list am-avg-lg-2 am-avg-md-2 am-avg-sm-2 clearFix">
                        {this.state.courseList.map((item,index)=>{
                          return(
-                           <CourseItem show={this.show.bind(this)} key={index} score={this.state.score} item={item} />
+                          <li key={index}>
+                          <a>
+                            <img onClick={()=>this.jump(item)} data-am-modal={item.goodCourse ==='1' && item.userCourseOperation.isBuy !== 1?`{target: '#course-confirm'}`:""} src={require('../../style/images/test.png')}/>
+                            <p onClick={()=>this.jump(item)} data-am-modal={item.goodCourse ==='1' && item.userCourseOperation.isBuy !== 1?`{target: '#course-confirm'}`:""} className="time">{tool.formatTimestamp(item.createTime)}</p>
+                          </a>
+                          <div className="cur-list-info">
+                            <h2 onClick={()=>this.jump(item)} data-am-modal={item.goodCourse ==='1' && item.userCourseOperation.isBuy !== 1?`{target: '#course-confirm'}`:""}>{item.courseName}{item.goodCourse === '1'?<div className="jc-icon" />:null}</h2>
+                            <p className="like">
+                              <span className={item.userCourseOperation.isCollection===1?'active':''} data-am-modal={item.goodCourse ==='1' && item.userCourseOperation.isBuy !== 1?`{target: '#course-confirm'}`:""} onClick={()=>this.click(item)}><i className="fa fa-heart-o"></i>{item.sumCollection}</span>
+                              <span className={item.userCourseOperation.isLike===1?'active':''} onClick={()=>this.action(item.courseId,3)}><i className="fa fa-thumbs-o-up"></i>{item.sumLike}</span>
+                              </p>
+                          </div>
+                          </li>
                            )
                        })}
                      </ul>
                      </div>
                      </Spin>
                      </div>
+
+         <div className="am-modal am-modal-confirm" tabIndex="-1" id={`course-confirm`}>
+					<div className="am-modal-dialog">
+						<div className="am-modal-hd">温馨提示</div>
+						<div className="am-modal-bd">
+						兑换课程将需要{this.state.now_item.studyIntegral}积分，您的当前积分为{this.state.score}，是否继续？
+						</div>
+						<div className="am-modal-footer">
+						<span className="am-modal-btn" data-am-modal-cancel>取消</span>
+						<span className="am-modal-btn" data-am-modal-confirm onClick={()=>this.ok()}>确定</span>
+						</div>
+						</div>
+					</div>
                   </div>
                 
               </div>
