91c6951e854ef9d742bac5fdbd547990ac27a4f3
QianLi <1290259180@qq.com>
2017-12-22 16:28:55 +0800

    图书馆去箭头

--- react/src/components/footer/Footer.jsx
+++ react/src/components/footer/Footer.jsx
@@ -33,7 +33,7 @@ class Footer extends React.Component {
     if(pathname === '/Course'){
       showFooter = true;
     }
-    if(pathname === '/Library'){
+    if(pathname === '/Library/1'){
       showFooter = true;
     }
     if(pathname === '/Personal'){
@@ -50,7 +50,7 @@ class Footer extends React.Component {
     if (pathname.indexOf('/Article') !== -1) {
       tab = 2;
     }
-    if (pathname.indexOf('/Library') !== -1) {
+    if (pathname.indexOf('/Library/1') !== -1) {
       tab = 4;
     }
     if (pathname.indexOf('/Personal') !== -1) {
@@ -92,7 +92,7 @@ class Footer extends React.Component {
             <li className={this.state.tab === 1?"li-1 active":"li-1"} ><Link onClick={()=>this.changeTab(1)}  to='/Index'> </Link></li>
             <li className={this.state.tab === 2?"li-2 active":"li-2"} ><Link className={localStorage.getItem('channelId')==='4'?'xxq':'fxq'} onClick={()=>this.changeTab(2)}  to='/Article' > </Link></li>
             <li className={this.state.tab === 3?"li-3 active":"li-3"} ><Link onClick={()=>this.changeTab(3)}  to='/Course' > </Link></li>
-            <li className={this.state.tab === 4?"li-4 active":"li-4"} ><Link onClick={()=>this.changeTab(4)}  to='/Library'></Link></li>
+            <li className={this.state.tab === 4?"li-4 active":"li-4"} ><Link onClick={()=>this.changeTab(4)}  to='/Library/1'></Link></li>
             <li className={this.state.tab === 5?"li-5 active":"li-5"} ><Link onClick={()=>this.changeTab(5)}  to='/Personal' > </Link></li>
           </ul>
         </footer>:null}</div>
